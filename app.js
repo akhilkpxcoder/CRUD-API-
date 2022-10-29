e34b0a41ce6d9fc5dcf87ee3dd115098e039414f
Akhil KP <98999402+akhilkpxcoder@users.noreply.github.com>
2022-10-29 19:12:32 +0530

    last commit

--- app.js
+++ app.js
@@ -1,24 +1,57 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const UserRouter = require("./src/router/user-router");
 
+const express = require('express');
+const cors = require('cors');
+const bodyparser = require('body-parser');
 const app = express();
-const PORT = 3000;
+const users = require("./src/router/user-router");
+const login = require("./src/router/login-router");
+const mongoose = require('mongoose');
+const docs = require('./docs');
+const db = require('./config/mongoose').dbcon;
 
-app.use(bodyParser.json());
-//to router
-app.use("/users", UserRouter);
+const swaggerUI = require("swagger-ui-express");
 
-//to home page
+// swaggerDocument = require("./swagger.json");
+// app.use(
+//   '/api-docs',
+//   swaggerUi.serve, 
+//   swaggerUi.setup(swaggerDocument)
+// );
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.info('MongoDb Connected'))
+  .catch(err => console.warn(err));
+
+app.use(cors());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+var options = {
+  explorer: true,
+  swaggerOptions: {
+    authAction: {
+      JWT: {
+        name: 'JWT',
+        schema: {
+          type: 'apiKey',
+          in: 'header',
+          name: 'Authorization',
+          description: ''
+        },
+        value: 'Bearer <my own JWT token>'
+      }
+    }
+  }
+};
+
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(docs,options));
+app.use('/api/users', users);
+app.use('/api/login', login);
 app.get("/", (req, res) => res.send("Welcome to the Users API!"));
 //not found error
 app.all("*", (req, res) => {
   res.status(404);
   res.send("You've tried reaching a route that doesn't exist.");
 });
-
-//server creation
-app.listen(PORT, () =>
-  console.log(`Server running on port: http://localhost:${PORT}`)
-);
-
+const PORT = 8081;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
